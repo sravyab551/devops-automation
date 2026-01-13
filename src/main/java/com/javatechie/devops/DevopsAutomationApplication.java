2e6c8e3db538c6e470d4f42b320dc333c00767c1
SRAVYA <sravya@bitvore.com>
2026-01-12 21:23:05 -0800

    first commit

---
+++ src/main/java/com/javatechie/devops/DevopsAutomationApplication.java
@@ -0,0 +1,20 @@
+package com.javatechie.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsAutomationApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome to DevOps Automation with Java Techie!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/devops/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
